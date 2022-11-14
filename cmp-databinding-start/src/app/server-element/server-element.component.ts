cd91426d08b4a7681d6b684098f2d7f8fe987105
= <=>
2022-11-14 15:32:41 -0500

    updating angular training

---
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -0,0 +1,46 @@
+import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css'], 
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class ServerElementComponent implements OnInit, OnChanges {
+ @Input('srvElement') element: {type: string, name: string, content: string};
+ @Input() name:string;
+@ViewChild('heading', {static:true}) header: ElementRef;
+  constructor() {
+    console.log('constructor called');
+   }
+
+   ngOnChanges(changes: SimpleChanges){
+ console.log('ngOnChanges called');
+ console.log(changes);
+   }
+  ngOnInit(): void {
+    console.log('ngOnInit called!');
+    console.log('Text Content: ' + this.header.nativeElement.textContent);
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheckCalled');
+  }
+ ngAfterContentInit(){
+  console.log('ngAfterContentInit Called');
+ } 
+ ngAfterContentChecked(){
+  console.log('ngAfterContentChecked called!');
+ }
+ ngAfterViewInit(){
+  console.log('ngAfterViewInit Called');
+  console.log('Text Content: ' + this.header.nativeElement.textContent);
+ } 
+ ngAfterViewChecked(){
+  console.log('ngAfterViewChecked called!');
+ }
+
+ ngOnDestroy(){
+  console.log('this.ngOnDestroy called');
+ }
+}
