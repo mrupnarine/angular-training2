37ab9fc4fda79314baa0b3319f43d9a92e8287f5
= <=>
2022-11-16 12:32:44 -0500

    Uodate Course Project

--- AngularCourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ AngularCourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -10,13 +10,19 @@ export class RecipeListComponent implements OnInit {
  recipes: Recipe[] = [
   new Recipe('A Test Recipe', 'This is simply a test',
   "https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/1:1/w_1920,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg" ), 
- new Recipe('A Test Recipe', 'This is simply a test', 
+ new Recipe('Another Test Recipe', 'This is simply a test', 
  "https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/1:1/w_1920,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg" )
 ];
+
+@Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe:Recipe)
+  {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
